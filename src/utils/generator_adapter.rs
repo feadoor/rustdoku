aefa643c9e93ba38c4458563983ab6ad7f62af06
Sam Cappleman-Lynes <sam.capplemanlynes@gmail.com>
2019-03-28 21:35:54 +0000

    Update to new generator syntax

--- src/utils/generator_adapter.rs
+++ src/utils/generator_adapter.rs
@@ -1,20 +1,22 @@
 //! An adapter that allows a generator to be used as an iterator.
 
+use std::marker::Unpin;
 use std::ops::{Generator, GeneratorState};
+use std::pin::Pin;
 
-pub struct GeneratorAdapter<G: Generator<Return = ()>>(G);
+pub struct GeneratorAdapter<G: Generator<Return = ()> + Unpin>(G);
 
-impl <G: Generator<Return = ()>> GeneratorAdapter<G> {
+impl <G: Generator<Return = ()> + Unpin> GeneratorAdapter<G> {
     pub fn of(generator: G) -> GeneratorAdapter<G> {
         GeneratorAdapter(generator)
     }
 }
 
-impl <G: Generator<Return = ()>> Iterator for GeneratorAdapter<G> {
+impl <G: Generator<Return = ()> + Unpin> Iterator for GeneratorAdapter<G> {
     type Item = G::Yield;
 
     fn next(&mut self) -> Option<Self::Item> {
-        match unsafe { self.0.resume() } {
+        match Pin::new(&mut self.0).resume() {
             GeneratorState::Yielded(x) => Some(x),
             GeneratorState::Complete(_) => None,
         }
