d31f1ce5a3166ed028b0dbdad7d62fb2ad1da1e3
Sam Cappleman-Lynes <sam.capplemanlynes@gmail.com>
2019-08-14 12:23:18 +0100

    Reset the generator to previous version

--- src/bin/generate.rs
+++ src/bin/generate.rs
@@ -11,20 +11,12 @@ use rustdoku::strategies::Strategy::*;
 
 fn main() {
 
-    let not_trivial = Criteria::not_solvable_with(SolveConfiguration::with_strategies(vec![
-        FullHouse, NakedSingle, HiddenSingle, Pointing, Claiming, HiddenSubset(2), HiddenSubset(3), 
-        HiddenSubset(4), NakedSubset(2), NakedSubset(3), NakedSubset(4), Fish(2), Fish(3), Fish(4),
-        FinnedFish(2), FinnedFish(3),
-    ]));
-
-    let solvable_with_advanced_technique = Criteria::solvable_with(SolveConfiguration::with_strategies(vec![
-        FullHouse, NakedSingle, HiddenSingle, FinnedFish(4),
-    ]));
+    let unsolvable = Criteria::not_solvable_with(SolveConfiguration::with_all_strategies());
 
     loop {
         let clues = generator::generate_minimal_symmetric_puzzle();
         let grid = Grid::from_clues(&clues).unwrap();
-        if analyser::meets_criteria(&grid, &solvable_with_advanced_technique) && analyser::meets_criteria(&grid, &not_trivial) {
+        if analyser::meets_criteria(&grid, &unsolvable) {
             println!("{}", clues.iter().map(|x| x.to_string()).collect::<String>());
         }
     }
