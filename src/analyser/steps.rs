352f604ca4c4c76d3fec9767d38c3ebbdf210125
Sam Cappleman-Lynes <sam.capplemanlynes@gmail.com>
2019-10-08 16:48:33 +0100

    Reset to base state

--- src/analyser/steps.rs
+++ src/analyser/steps.rs
@@ -9,19 +9,19 @@ use strategies::{Deduction, Step, Strategy};
 /// finding the first group that contains a technique that can be applied to
 /// the puzzle, finding all deductions arising from techniques in that group,
 /// and applying them all simultaneously.
-pub fn steps_to_solve(grid: &Grid, strategies: &[Vec<Strategy>]) -> Option<usize> {
+pub fn steps_to_solve(grid: &Grid, strategies: &[Vec<Strategy>]) -> Option<Vec<usize>> {
 
-    let mut steps_taken = 0; let mut working_grid = grid.clone();
+    let mut steps_taken = vec![0; strategies.len()]; let mut working_grid = grid.clone();
     'outer: while !working_grid.is_solved() {
 
-        for strategy_group in strategies {
+        for (idx, strategy_group) in strategies.iter().enumerate() {
             let moves: Vec<Step> = strategy_group.iter().flat_map(|strat| strat.find_steps(&working_grid)).collect();
             let deductions: Vec<Deduction> = moves.iter().flat_map(|mov| mov.get_deductions(&working_grid)).collect();
             if !deductions.is_empty() {
                 for deduction in deductions {
                     working_grid.apply_deduction(deduction);
                 }
-                steps_taken += 1; continue 'outer;
+                steps_taken[idx] += 1; continue 'outer;
             }
         }
 
