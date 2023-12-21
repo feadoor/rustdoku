23c9be5369e131e787d0bcef7383628f94947bae
Sam Cappleman-Lynes <sam.capplemanlynes@gmail.com>
2023-12-21 00:56:29 +0000

    Update to latest nightly

--- src/utils/generator_adapter.rs
+++ src/utils/generator_adapter.rs
@@ -1,24 +1,24 @@
 //! An adapter that allows a generator to be used as an iterator.
 
 use std::marker::Unpin;
-use std::ops::{Generator, GeneratorState};
+use std::ops::{Coroutine, CoroutineState};
 use std::pin::Pin;
 
-pub struct GeneratorAdapter<G: Generator<Return = ()> + Unpin>(G);
+pub struct GeneratorAdapter<G: Coroutine<Return = ()> + Unpin>(G);
 
-impl <G: Generator<Return = ()> + Unpin> GeneratorAdapter<G> {
+impl <G: Coroutine<Return = ()> + Unpin> GeneratorAdapter<G> {
     pub fn of(generator: G) -> GeneratorAdapter<G> {
         GeneratorAdapter(generator)
     }
 }
 
-impl <G: Generator<Return = ()> + Unpin> Iterator for GeneratorAdapter<G> {
+impl <G: Coroutine<Return = ()> + Unpin> Iterator for GeneratorAdapter<G> {
     type Item = G::Yield;
 
     fn next(&mut self) -> Option<Self::Item> {
         match Pin::new(&mut self.0).resume(()) {
-            GeneratorState::Yielded(x) => Some(x),
-            GeneratorState::Complete(_) => None,
+            CoroutineState::Yielded(x) => Some(x),
+            CoroutineState::Complete(_) => None,
         }
     }
 }
